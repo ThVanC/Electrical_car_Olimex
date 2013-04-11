f4707a7bc6f7e8c82bbbdf6ff82d873dfba013a2
flipomelo <filip.vr@hotmail.com>
2013-04-11 16:55:13 +0300

    Update controller_car.h
    loadfactor invoeren

--- controller_car.h
+++ controller_car.h
@@ -14,6 +14,7 @@ int voltage;//De spanning over de batterij uitgedrukt in mV
 int state_of_charge;//De batterijstatus uitgedrukt in procent
 int temperature;
 int max_temp=50;//de default maximum temperatuur van de wagen
+int load=0; //Deze waarde is een indicatie van hoe sterk de wagen mag opladen, als deze waarde positief is gaat de wagen opladen, anders ontladen. Uitgedrukt in milliwatt?
 
 int getVoltage();
 void setVoltage(int i);
