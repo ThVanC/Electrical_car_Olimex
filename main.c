a13b0afe343840c528ea8b4d20cce73225b35992
Zeger Van de Vannet <zegervdv@me.com>
2013-04-11 09:51:31 +0200

    Verwijdert int voor de call naar pthread

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ void main(){
 	//niet te vergeten bij het compilen in linux: gcc mai.c -o main -l pthread 
 	//We gaan enkele threads aanmmaken: één thread voor de meting en 1 thread voor het rijden of opladen. (de auto kan niet rijden en opladen tegelijk)
 	pthread_t draad;
-	int pthread_create(&draad,NULL,*meting(), NULL);
-	int pthread_create(&draad,NULL,*client(), NULL);
-	int pthread_create(&draad,NULL,*initDrive(), NULL);
+	pthread_create(&draad,NULL,*meting(), NULL);
+	pthread_create(&draad,NULL,*client(), NULL);
+	pthread_create(&draad,NULL,*initDrive(), NULL);
 	return;
 	//int pthread_create(pthread_t * pth, pthread_attr_t *attr, void* (*function), void * arg);
 	
