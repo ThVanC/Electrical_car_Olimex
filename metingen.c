099cbb345c7e62a7c086be0e26014a75774287ca
Zeger Van de Vannet <zegervdv@me.com>
2013-04-15 18:21:42 +0200

    Gebruik constanten voor weerstandswaarden

--- metingen.c
+++ metingen.c
@@ -11,7 +11,7 @@ int measureV(){
 	int V_LRADC1,V_bat;//spanningen in mV
 	
 	V_LRADC1=readLRADC1();
-	V_bat=(V_LRADC1*236)/56;//236 owv 56+180 ; haakjes zo zetten zal best nauwkeurigheid geven
+	V_bat=(V_LRADC1*(R4 + R5)) / R5; //236 owv 56+180 ; haakjes zo zetten zal best nauwkeurigheid geven
 	
 	return V_bat;
 }
@@ -21,7 +21,7 @@ int measureI(){
 	int curr;//stromen in mA
 
 	V_LRADC0=readLRADC0();
-	V_Hall=(V_LRADC0*340)/120;//340 owv 120+220 ; haakjes zo zetten zal best nauwkeurigheid geven
+	V_Hall=(V_LRADC0*(R6 + R3)) / R6;//340 owv 120+220 ; haakjes zo zetten zal best nauwkeurigheid geven
 	
 	//positieve stroom is opladen, negatieve is ontladen
 	curr=(V_Hall-2500)*10; //want 100mV/A => 10mA/mV
--- metingen.h
+++ metingen.h
@@ -1,6 +1,13 @@
 #ifndef METING_H
 #define METING_H
 
+// Definitie weerstandswaarden (kOhm)
+#define R3	220
+#define R4	180
+#define R5	56
+#define R6	220
+
+
 int initMeasurements();
 
 //spanningsmeting: LRADC1 pin32
