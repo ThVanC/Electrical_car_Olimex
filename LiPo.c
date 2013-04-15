617539fff6542569eaf2c56048f558677d0eeace
Zeger Van de Vannet <zegervdv@me.com>
2013-04-15 17:09:21 +0200

    LiPo - charger algoritme
    Het laadalgoritme voor LiPo-batterijen in twee fasen:
        - opladen met constante stroom tot spanning is bereikt
        - stapsgewijs verminderen van stroom iedere keer spanning maximaal

---
+++ LiPo.c
@@ -0,0 +1,63 @@
+#include "lipo.h"
+#include "metingen.h"
+#include <time.h>
+
+#define	V_THRESHOLD_CELL 	4200	// Tresholdspanning voor een cel (mV)
+#define	C 			4000 	// Stroomcapaciteit (mAh)
+#define	NR_OF_CELLS		2 	// Aantal cellen in parallel
+
+#define MARGE			0.99 	// Marge-factor
+
+// Teller voor oplaadbeurten
+global int k = 1;
+
+int charge_LiPo(){
+	// Definitie van variabelen
+	int voltage;
+	int current;
+	struct timespec sleepTime;
+	
+	// Slaaptijd tussen opeenvolgende metingen, in milliseconden
+	sleepTime.tv_sec = 0;
+	sleepTime.tv_nsec = 100 * 1000000L;
+
+	// Eerste fase:
+	// Legt een constante stroom aan zolang de spanning onder de V_treshold blijft.
+	do {	
+		// Meet huidige spanning
+		voltage = measureV();
+		// Stel de stroom in op 1 x C
+		current = 1*C;
+		setCurrent(current);
+		
+		// Wacht enkele milliseconden vooraleer volgende meting uit te voeren.
+		nanosleep(&sleepTime, (struct timespec *) NULL);
+	} while (voltage < (V_THRESHOLD_CELL*NR_OF_CELLS)*MARGE);
+
+
+	// Meet nu zowel stroom als spanning
+	voltage = measureV();
+	current = measureI();
+
+	// Tweede fase:
+	// Laat de stroom afnemen.
+	// Wacht totdat de spanning terug V_threshold is en herhaal
+	// Stroom neemt per iteratie 10% af
+	while (current > (1-MARGE)*C) {
+		// Als spanning ongeveer V_threshold is, verlaag de stroom
+		if (voltage > (V_THRESHOLD_CELL*NR_OF_CELLS)*MARGE) k++;
+		
+		// Stel stroom in
+		current = C/(1-k/10);
+		setCurrent(current);
+
+		// Meet de nieuwe spanning
+		voltage = measureV();
+
+		// Wacht enkele milliseconden tot volgende meting
+		nanosleep(&sleepTime, (struct timespec *) NULL);
+	}
+	
+	// Opladen voltooid
+	return 1; 
+}
--- LiPo.h
+++ LiPo.h
@@ -1,6 +1,8 @@
 #ifndef LIPO_H
 #define LIPO_H
 
+// Laadalgoritme voor LiPo batterijen
+// return: 1 als opladen voltooid is.
 int charge_LiPo();
 
 #endif
