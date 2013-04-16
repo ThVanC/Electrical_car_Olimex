0cf77ca65a0250d6570e2d337a0f0c904bc3f3b9
Thomas Van Cleemput <thomas.van.cleemput@gmail.com>
2013-04-16 12:19:17 +0300

    Update GPS.h
    We gaan de verschillende GPS-nodes bijhouden in een array van GPS_Nodes. Daarbij moet ook het aantal nodes die te rijden zijn bijgehouden worden anders kunnen we niet achterhalen hoe lang die rij is.

--- GPS.h
+++ GPS.h
@@ -1,12 +1,20 @@
+#ifndef GPS_H
+#define GPS_H
+
 enum direction{
 forward,
 left,
 right,
 backward
 }
+
 typedev struct GPS_node{
 	int max_speed;
 	direction dir;
 	int distance;
 };
+
+int aantal_nodes;
+GPS_node* track;
 	
+#endif
