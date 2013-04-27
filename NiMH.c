4ca037e08f23698968730daaa42188418a2a6e56
laurens b <laurens.bogaert@hotmail.com>
2013-04-27 17:56:28 +0200

    update NiMH
    - usleep ipv nanosleep
    - limiet bijgevoegd

--- NiMH.c
+++ NiMH.c
@@ -3,25 +3,25 @@
 #include "metingen.h"
 #include <stdio.h>
 #include "NiMH.h"
-#include <time.h>
+#include <unistd.h>
+
 
 void init_V_max(){
 	V_max=0;
 }
 
-int charge_NiMH(){
+int charge_NiMH(int socLoad){
 	int V_bat;
-	setCurrent(C);
-
-	struct timespec sleepTime_NiMH,sleepTime_off;
+	int sleepTime,sleepTime_fin;
 	
+	init_V_max();
+	setCurrent(C);
 	// Slaaptijd tussen opeenvolgende metingen, in milliseconden
-	sleepTime_NiMH.tv_sec = 0;
-	sleepTime_NiMH.tv_nsec = 100 * 1000000L;
-	sleepTime_off.tv_sec = 5;
-	sleepTime_off.tv_nsec = 0;
+	sleepTime = 100;
+	sleepTime_fin =5000;
 
-	while (on==1) {
+	while (on==1 && charging==1) {
+		if (isAtChargeLimit(socLoad)) return 2;
 		V_bat=measureV();
 	
 		if(V_max<V_bat){
@@ -30,13 +30,13 @@ int charge_NiMH(){
 	
 		if(V_bat<V_max*0.9975){
 			//hoe kan je controleren of dit 5 sec lang zo blijft (andere tijdsduur is ook goed, maar men stelt 5 sec voor)
-			nanosleep(&sleepTime_off, (struct timespec *) NULL);
+			usleep(sleepTime_fin*1000);
 			if(V_bat<V_max*0.9975){
 				return 1;
 			}
 		}
 
-		nanosleep(&sleepTime_NiMH, (struct timespec *) NULL);
+		usleep(sleepTime*1000);
 	}
-	
+	return 0;
 }
--- NiMH.h
+++ NiMH.h
@@ -6,7 +6,11 @@ int V_max; //in mV de maximale spanning in deze sessie
 //DON'T FORGET: init_V_max voor elke oplaadsessie !!!
 
 void init_V_max();
-int charge_NiMH();
+
+// return: 2 opladen gestopt omdat SoC (<100) is bereikt
+// return: 1 als opladen voltooid is.
+// return: 0 voor charging == 0 of laden == off
+int charge_NiMH(int socLoad);
 
 #endif
 
