b8a3f53d2a34183edd2367b7e4545c46699a24ac
Zeger Van de Vannet <zegervdv@me.com>
2013-04-27 14:54:10 +0200

    Uitwerking I2C lezen en schrijven
    zie: https://github.com/OLIMEX/OLINUXINO/blob/master/SOFTWARE/iMX233/I2C/readi2c.c

---
+++ i2c.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <linux/i2c-dev.h>
+
+
+void i2c_init(char* device){
+        // TODO: testen en/of aanpassen!
+        // Open het I2C device 
+        deviceDescriptor = open(device, O_RDWR);
+        if (deviceDescriptor == -1){
+                printf("Fout bij het openen van %s \n", device);
+                break;
+        }
+}
+
+void i2c_read_data(unsigned char* addr, unsigned char* data, int length){
+        if(ioctl(deviceDescriptor,I2C_SLAVE, addr))
+		printf("I2cReadData_device : IOCTL Problem");
+
+        read(deviceDescriptor, data, length);
+}
+
+void i2c_write_data(unsigned char addr, unsigned char* data, int length){
+        if(ioctl(deviceDescriptor,I2C_SLAVE, addr))
+		printf("I2cSendData_device : IOCTL Problem");
+
+        write(deviceDescriptor, data, length);
+}
+
+// Wrapper functie: gaat ervan uit dat i2c_init al is opgeroepen
+void i2c_read_temp(unsigned char* msbyte, unsigned char* lsbyte){
+        unsigned char* data[2];
+        i2c_read_data(ADDR_TEMPSENSOR, data, 2);
+        // Aanpassen van MSByte en LSByte
+        *msbyte = data[0];
+        *lsbyte = data[1];
+}
